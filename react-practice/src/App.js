efc2957b8395fa74edf11a7bb076ac05282e4a94
Menyhárt László <senkialfonz11@gmail.com>
2022-09-14 09:46:43 +0200

    React Ajax

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -2,23 +2,21 @@ import React, { useEffect, useState } from 'react';
 
 export default function App() {
 
-  const [list, setList] = useState( [] )
+  const [list, setList] = useState([])
 
-  useEffect( () => {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then( (res) => {
-      return res.json();
-    })
-    .then( (res) => {
-    setList( res );
-    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setList(res);
+      })
   }, []);
-
-
 
   return (
     <div className='container'>
-      { list.map( (item, index) => {
+      {list.map((item, index) => {
         return <React.Fragment key={index}>
           <h3>{item.title}</h3>
           <p>{item.body}</p>
