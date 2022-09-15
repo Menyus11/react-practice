fc0043aa4bb76e08da8e421aa0cbe1ee042f3ca1
Menyhárt László <senkialfonz11@gmail.com>
2022-09-15 19:52:42 +0200

    React learning

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,29 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css'
 
-export default function App() {
+const App = () => {
 
-  const [list, setList] = useState([])
+  const [data, setData] = useState( {name: '', email: ''} );
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        setList(res);
-      })
-  }, []);
+  return(
+    <div className='container p-5 row' style={{background: "pink"}}>
+      <div className='col'></div>
+      <form className='col-12' onSubmit={ ( e ) => {
+        e.preventDefault();
+        fetch( './feldolgozo.php', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify( data )
+        })
+      }}>
 
-  return (
-    <div className='container'>
-      {list.map((item, index) => {
-        return <React.Fragment key={index}>
-          <h3>{item.title}</h3>
-          <p>{item.body}</p>
-          <hr />
-        </React.Fragment>
-      })}
+        <div className='text-center m-3'>
+          <label htmlFor='name'>Kérjük írja be a felhasználónevét!</label>
+          <input type="text" className="form-control" id="name" onChange={ (e) => setData({ ...data, name: e.target.value}) }/>
+        </div>
+
+        <div className='text-center m-3'>
+          <label htmlFor='email'>Kérjük írja be az email címét!</label>
+          <input type="text" className="form-control" id="email" onChange={ (e) => setData({ ...data, email: e.target.value}) }/>
+        </div>
+
+        <div>
+          <button type="submit" className='btn btn-success form-control'>Küldés</button>
+        </div>
+      
+      </form>
+      <div className='col'></div>
+      {/* <br /><p>név: {data.name}</p><br /><p>email: {data.email}</p> */}
     </div>
-  )
 
+  )
 }
+
+export default App;
